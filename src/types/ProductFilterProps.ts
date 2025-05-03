04d14a13df0c0a06ea7a4ab9275f4151b626d786
nazarejose <nazarejosecontact@gmail.com>
2025-05-03 18:25:14 -0300

    First Commit

---
+++ src/types/ProductFilterProps.ts
@@ -0,0 +1,24 @@
+/*
+
+type objectProps = {
+    mark : markEnum[]
+    isNew : boolean
+    category : string[]
+    gender : string[]
+}
+
+enum markEnum {
+    Addidas = "Addidas"
+
+}
+
+markEnum.Addidas
+
+ const sapato : objectProps = {
+    mark : [markEnum.Addidas],
+    isNew: true,
+    category: ["Esportivo"],
+    gender: ["Masculino"]
+}
+*/
+
